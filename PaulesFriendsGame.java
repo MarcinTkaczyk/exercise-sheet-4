91acc8b7cd9a5505492a8b2125acd301b00cc8bd
Tim-Julian Ehret <tjehret@gmx.de>
2020-11-23 13:38:48 +0100

    Update PaulesFriendsGame to fix format issues
    This should conform to eclipse auto-formatting

--- PaulesFriendsGame.java
+++ PaulesFriendsGame.java
@@ -3,30 +3,25 @@
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Location;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.datatypes.Direction;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Hamster;
-//import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.SimpleHamsterGame;
 import de.unistuttgart.iste.rss.oo.hamstersimulator.external.model.Territory;
 
 public class PaulesFriendsGame extends Exercise04Base {
 
-    /**
-     * Put your hamster code into this operation.
-     * Solve the task in this operation.
-     */
+	/**
+	 * Put your hamster code into this operation. Solve the task in this operation.
+	 */
 	void createPaulesFriends() {
-		//TODO: implement according to exercise 2 of sheet 4
+		// TODO: implement according to exercise 2 of sheet 4
 
-		//Hint: Be careful with the location of the new hamsters
+		// Hint: Be careful with the location of the new hamsters
 
-		
 	}
-	
+
 	/**
 	 * Do not modify this operation.
 	 */
-	void run(){
-	game.displayInNewGameWindow();
-	createPaulesFriends();
-}
-
-
+	void run() {
+		game.displayInNewGameWindow();
+		createPaulesFriends();
+	}
 }
